e06a74d6b52b2e31ace2086fba7e039c011f5fe1
Watchcoin Ventures <156217571+K1mc4n@users.noreply.github.com>
2025-06-30 21:04:55 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Anda bisa menambahkan domain lain di sini jika diperlukan
+    ],
+  },
 };
 
 export default nextConfig;
