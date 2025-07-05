9da0addba523666a13c8e79ef2a42817c05e8e55
Watchcoin Ventures <156217571+K1mc4n@users.noreply.github.com>
2025-07-06 00:54:09 +0700

    Update providers.tsx

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,24 +1,16 @@
+// src/app/providers.tsx
+
 "use client";
 
-import React from "react";
+import { MiniAppProvider } from "@neynar/react";
 import WagmiProvider from "~/components/providers/WagmiProvider";
-import { NeynarProvider } from "@neynar/react";
-import { APP_URL } from "~/lib/constants";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const neynarClientId = process.env.NEXT_PUBLIC_NEYNAR_CLIENT_ID;
-
-  if (!neynarClientId) {
-    console.error("Neynar Client ID is not configured. Features will be limited.");
-    return <WagmiProvider>{children}</WagmiProvider>; 
-  }
-  
   return (
-    <NeynarProvider
-      clientId={neynarClientId}
-      domain={new URL(APP_URL).hostname}
-    >
-      <WagmiProvider>{children}</WagmiProvider>
-    </NeynarProvider>
+    <WagmiProvider>
+      <MiniAppProvider>
+        {children}
+      </MiniAppProvider>
+    </WagmiProvider>
   );
 }
