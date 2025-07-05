cd32e192a221dae2839d533b97e92205067929a8
Watchcoin Ventures <156217571+K1mc4n@users.noreply.github.com>
2025-07-05 17:19:10 +0700

    Update textarea.tsx

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,10 +1,8 @@
 // src/components/ui/textarea.tsx
-
 import * as React from "react"
 import { cn } from "~/lib/utils"
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
