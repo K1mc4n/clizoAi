7fe774df509024befc3af4a853e9869482361bb0
Watchcoin Ventures <156217571+K1mc4n@users.noreply.github.com>
2025-07-05 20:20:20 +0700

    Delete postcss.config.mjs

--- postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Opsi ini untuk menonaktifkan Terser yang menyebabkan error.
-  // Ini adalah jalan terakhir untuk melewati bug build yang spesifik.
-  swcMinify: false,
-};
-
-export default nextConfig;
